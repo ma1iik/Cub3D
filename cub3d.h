dc1519354d049aa41e3bb8eb07934cb95859dc56
ma1iik <misrailo@student.42nice.fr>
2023-01-23 17:56:46 +0100

    walls check left

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ma1iik <ma1iik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 02:27:49 by ma1iik            #+#    #+#             */
-/*   Updated: 2023/01/22 00:13:14 by ma1iik           ###   ########.fr       */
+/*   Updated: 2023/01/22 13:18:34 by ma1iik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,6 @@ int ft_check_ext(t_data *data, char *file);
 int ft_fill_map(t_data *data, int fd);
 int	ft_error(int n);
 int	ft_mapsize(char *file);
+int	ft_wrongchar(t_data *data);
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ma1iik <ma1iik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 01:27:23 by ma1iik            #+#    #+#             */
-/*   Updated: 2023/01/22 13:09:16 by ma1iik           ###   ########.fr       */
+/*   Updated: 2023/01/23 06:19:46 by ma1iik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,10 @@ int	ft_error(int n)
 		printf("Error : Map file is a directory\n");
 	else if (n == 3)
 		printf("Error : Map file not read properly\n");
+	if (n == 4)
+		printf("Error : Wrong character on map found\n");
+	if (n == 5)
+		printf("Error : Direction utilized in incorrect manner \n");
 	return (WRONG);
 }
 
@@ -84,7 +88,7 @@ int	ft_fill_map(t_data *data, int fd)
 	while (gnl > 0)
 	{
 		gnl = get_next_line(fd, &line);
-		if (line[0] == 'N' || line[0] == 'S' || line[0] == 'W' || line[0] == 'E' || line[0] == 'N' || line[0] == '\n' || line[0] == '\0')
+		if (line[0] == 'N' || line[0] == 'S' || line[0] == 'W' || line[0] == 'E' || line[0] == 'F' || line[0] == 'C' || line[0] == '\n' || line[0] == '\0')
 		{
 			free (line);
 			continue;
@@ -94,6 +98,53 @@ int	ft_fill_map(t_data *data, int fd)
 	}
 	if (i == 0)
 		return (ft_error(3));
+	if (ft_fill_map2(data, fd) == WRONG)
+		return (WRONG);
+	return (RIGHT);
+}
+
+int	ft_map_len(t_data *data)
+{
+	int	len;
+	int	i;
+	int	j;
+
+	i = 0;
+	len = 0;
+	while (data->map[i])
+	{
+		j = 0;
+		while (data->map[i][j])
+		{
+			j++;
+		}
+		if (j > len)
+			len = j;
+		i++;
+	}
+	return (len);
+}
+
+int	ft_fill_map2(t_data *data, int fd)
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	i = 0;
+	while (data->map[i][j])
+	{
+		j = 0;
+		tmp = ft_calloc(ft_map_len(data) + 1, sizeof(char));
+		if (!tmp)
+			return (WRONG);
+		tmp[j] = data->map[i][j];
+		j++;
+		while (j < ft_map_len(data))
+			tmp[j++] = '\0';
+		free (data->map[i]);
+		data->map[i] = tmp;
+	}
 	return (RIGHT);
 }
 
@@ -122,6 +173,34 @@ int ft_check_ext(t_data *data, char *file)
 	return (RIGHT);
 }
 
+int	ft_wrongchar(t_data *data)
+{
+	int	i;
+	int	j;
+	int	num;
+
+	i = 0;
+	num = 0;
+	while(data->map[i])
+	{
+		j = 0;
+		while(data->map[i][j])
+		{
+			if (data->map[i][j] != '1' && data->map[i][j] != '0' && data->map[i][j] != 'N'
+			&& data->map[i][j] != 'S' && data->map[i][j] != 'W' && data->map[i][j] != 'E' && data->map[i][j] != ' ')
+				return(ft_error(4));
+			if (data->map[i][j] == 'N' || data->map[i][j] == 'S' || data->map[i][j] == 'W' || data->map[i][j] == 'E')
+				num++;
+			j++;
+		}
+		i++;
+	}
+	if (num != 1)
+		return (ft_error(5));
+	return (RIGHT);
+}
+
+
 int ft_parsing(int ac, t_data *data, char *file)
 {
     int     fd;
@@ -138,21 +217,10 @@ int ft_parsing(int ac, t_data *data, char *file)
 		return(WRONG);
     if (ft_fill_map(data, fd) == WRONG)
 		return (WRONG);
-	if (ft_wrongchar(data, fd) == WRONG)
+	if (ft_wrongchar(data) == WRONG)
 		return (WRONG);
 	close(fd);
     return (RIGHT);
-}
-
-int	ft_wrongchar(t_data *data, int fd)
-{
-	int	i = 0;
-
-	while(data->map[i] )
-	{
-		printf("%s\n", data->map[i]);
-		i++;
-	}
 }
 
 int main(int ac, char **av)
